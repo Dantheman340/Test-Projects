aa5721f159803ee2ca5df77a2c8d6b40ab555f7c
Daniel Joensen <dantheman340@gmail.com>
2015-02-08 22:58:03 -0500

    adding another test file and modifying the original

--- python/test2.py
+++ python/test2.py
@@ -1,9 +1,5 @@
-#distance = -5.0
-#print(type(distance))
-#print(abs(distance))
-
-distance = -5
 def distance_from_zero(distance):
+    distance = -5
     if type(distance) == int or type(distance) == float:
         return abs(distance)
     else:
